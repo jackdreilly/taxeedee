94fadf8afa4f4edaaaa70a6af4d3667fae0003fa
jackdreilly <jackdreilly@gmail.com>
2018-06-25 05:13:02 -0700

    first commit

---
+++ map.js
@@ -0,0 +1,18 @@
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 0, lng: 0 },
+        zoom: 1
+    });
+    map.data.loadGeoJson("locations.json");
+    map.data.addListener('click', function(event) {
+        const infoWindow = new google.maps.InfoWindow({
+    		content: event.feature.getProperty("city"),
+  		});
+  		const position = event.feature.getGeometry().get();
+  		infoWindow.setPosition(position);
+  		infoWindow.setOptions({pixelOffset: new google.maps.Size(0,-30)});
+  		infoWindow.open(map);
+    });
+}
