1f30f0cbc937fe6b3f0a51b0f4e8dc16dc12e60b
Jack Reilly <jackdreilly@gmail.com>
2018-08-18 12:15:55 +0300

    added city and country.

--- Dockerfile
+++ Dockerfile
@@ -18,5 +18,6 @@ ENV STATIC_FOLDER static
 ENV CLIENT_ADDR server:50051
 ENV port 5000
 EXPOSE 5000
+RUN pip install --upgrade taxeedee_service
 ENTRYPOINT ["python"]
 CMD ["web.py"]
--- html/src/index.js
+++ html/src/index.js
@@ -11,8 +11,8 @@ function resetEllipses() {
 }
 
 function loadPosts(response) {
-	response.posts.map(addPost);
-	resetEllipses();
+  response.posts.map(addPost);
+  resetEllipses();
 }
 
 const postHtml = `
@@ -20,15 +20,15 @@ const postHtml = `
   <h2 class="title-text"></h2>
   <div class="img">
     <img class="square-image" />
-    <h3 class="location-text"></h3>
+    <h3 class="location-text"><span class="city-text"></span>, <span class="country-text"></span></h3>
   </div>
   <div class="text-container read-more-container">
     <div class="text collapse">
-		</div>
-  	<button class="more-text">Expand Caption</button>
+    </div>
+    <button class="more-text">Expand Caption</button>
   </div>
   <div class="comments-container read-more-container">
-	  <h3>Comments</h3>
+    <h3>Comments</h3>
     <div class="add-comment">
       <form name="comment_form">
         <label for="user">Name</label>
@@ -40,32 +40,33 @@ const postHtml = `
         <button class="submit">Submit</button>
       </form>
     </div>
-	  <div class="comments collapse">
-	  </div>
-  	<button type="submit" class="more-text">Expand Comments</button>	  
-	</div>
+    <div class="comments collapse">
+    </div>
+    <button type="submit" class="more-text">Expand Comments</button>    
+  </div>
 </div>
 `;
 
 function parseHtml(domString) {
-	const parser = new DOMParser();
-	const html = parser.parseFromString(domString, 'text/html');    
-	return html.body.firstChild;
+  const parser = new DOMParser();
+  const html = parser.parseFromString(domString, 'text/html');    
+  return html.body.firstChild;
 }
 
 function addPost(post) {
-	const node = parseHtml(postHtml);
-	node.setAttribute('data-post_id', post.id);
-	node.querySelector('img').setAttribute('src', post.photo.url);
-	node.querySelector('.title-text').innerText = post.title;
-	node.querySelector('.location-text').innerText = post.title;
-	node.querySelector('.text').innerHTML = post.content;
-	addComments(node.querySelector('.comments'), post.comments);
-	addPostNode(node);
+  const node = parseHtml(postHtml);
+  node.setAttribute('data-post_id', post.id);
+  node.querySelector('img').setAttribute('src', post.photo.url);
+  node.querySelector('.title-text').innerText = post.title;
+  node.querySelector('.city-text').innerText = post.location.city;
+  node.querySelector('.country-text').innerText = post.location.country;
+  node.querySelector('.text').innerHTML = post.content;
+  addComments(node.querySelector('.comments'), post.comments);
+  addPostNode(node);
 }
 
 function addComments(node, comments) {
-	comments.map(comment => addComment(node, comment));
+  comments.map(comment => addComment(node, comment));
 }
 
 const commentHtml = `
@@ -80,48 +81,48 @@ const commentHtml = `
 `
 
 function addComment(topNode, comment) {
-	const node = parseHtml(commentHtml);
-	node.querySelector('.comment-title').innerText = comment.name;
-	node.querySelector('.content').innerText = comment.comment;
-	node.querySelector('.time').innerText = moment(comment.timestamp).calendar();
-	topNode.appendChild(node);
+  const node = parseHtml(commentHtml);
+  node.querySelector('.comment-title').innerText = comment.name;
+  node.querySelector('.content').innerText = comment.comment;
+  node.querySelector('.time').innerText = moment(comment.timestamp).calendar();
+  topNode.appendChild(node);
 }
 
 const postsSelector = '#stream';
 
 function addPostNode(node) {
-	document.querySelector(postsSelector).appendChild(node);
+  document.querySelector(postsSelector).appendChild(node);
 }
 
 function handleAddCommentResponse(target, response) {
-	const commentsNode = target.closest('.comments-container').querySelector('.comments');
-	commentsNode.innerHTML = '';
-	addComments(commentsNode, response.comments);
+  const commentsNode = target.closest('.comments-container').querySelector('.comments');
+  commentsNode.innerHTML = '';
+  addComments(commentsNode, response.comments);
 }
 
 function postId(node) {
-	return node.closest('.post').dataset.post_id;
+  return node.closest('.post').dataset.post_id;
 }
 
 function listenForComments() {
-	on('click', 'form[name="comment_form"] button.submit', event => {
-		const target = event.currentTarget;
-		event.preventDefault();
-		const form = target.closest('form');
-		fetch('/add_post_comment', {
-			method: 'post',
-			headers: {
+  on('click', 'form[name="comment_form"] button.submit', event => {
+    const target = event.currentTarget;
+    event.preventDefault();
+    const form = target.closest('form');
+    fetch('/add_post_comment', {
+      method: 'post',
+      headers: {
         "Content-Type": "application/json; charset=utf-8",
       },
-			body: JSON.stringify({
-				'post_id': postId(target),
-				'name': form.elements.user.value,
-				'comment': form.elements.content.value
-			})
-		})
-			.then(response => response.json())
-			.then(response => handleAddCommentResponse(target, response));
-	});
+      body: JSON.stringify({
+        'post_id': postId(target),
+        'name': form.elements.user.value,
+        'comment': form.elements.content.value
+      })
+    })
+      .then(response => response.json())
+      .then(response => handleAddCommentResponse(target, response));
+  });
 }
 
 
@@ -131,7 +132,7 @@ window.onload = () => {
       .then(loadPosts);
   listenForComments();
   on('click', '.more-text', e => {
-  	$(e.currentTarget).hide();
+    $(e.currentTarget).hide();
     const text = $(e.currentTarget).closest(".read-more-container").find(".collapse");
     text.removeClass("collapse");
     text.dotdotdot({}).data("dotdotdot").restore();
