f6d1e9efdab3ee3a5dd2058cf8e6e7be58a89cca
Olga Kavvada <okavvada@gmail.com>
2018-08-18 14:52:37 +0300

    delete test post

--- add_data.py
+++ add_data.py
@@ -2,13 +2,6 @@ from taxeedee_service.client import Client
 c = Client()
 c.clear_db()
 c.add_post(
-	title = 'Test Title',
-	city = 'Test City',
-	country = 'Test Country',
-	url = 'https://lh3.googleusercontent.com/r_9AMLGjqJ59y8D5qjIb3-YmDCWEHgaIe1LXqp40ZM471dV1TyG0pZFKWn0bDhfnNyLqT9szzgHqlIFjSfExKVzXjFdF163CvrK47P77K0laHPzWezAGEYy49lV6vgXYAV4LFZbrf5c=w600-h315-p-k',
-	content = 'My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content My Test content'
-	)
-c.add_post(
     title = 'First Stop: Lebanon',
     city = 'Beirut',
     country = 'Lebanon',
