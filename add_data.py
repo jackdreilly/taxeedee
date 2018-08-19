647564c8cfd00afbb932fe0830e0edcaf83d925e
Olga Kavvada <okavvada@gmail.com>
2018-08-19 12:24:40 +0300

    text add

--- add_data.py
+++ add_data.py
@@ -8,22 +8,35 @@ c.add_post(
     url = 'https://lh3.googleusercontent.com/gLOpqEE7ewilIqYjq3rQkRA2cG4MWGBL7_-Ew2ndX8yGM9r7G-NRTlsqE4hgccjU_9bmcD21DlVq6nn-KjydhGuTjbO4DjJps6drEU1M7tE8cl_sZz0TeKiXEYFU3i970Xc1jfblA3E=w600-h315-p-k',
     content = """
               <p>I am really excited to be writing our first post. We have been putting a lot of love into the website design that it is already a month in our trip and we have yet to make a post. But this is it! 
-              <p>The main motivation behind the entire trip and the event that signified the official deadline for us to have everything ready and packed to go was our friends Tala and Marwan's wedding in Beirut. We have been talking about attending this event for months and it gave us momentum to continue on our journey.
+              <p>The main motivation behind the entire trip and the event that signified the official deadline 
+              for us to have everything ready and packed to go was our friends Tala and Marwan's wedding in Beirut. 
+              We have been talking about attending this event for months and it gave us momentum to continue on our journey.
               <div class="img">
                 <img class="square-image" src="https://lh3.googleusercontent.com/-78zqPHmdTQ5MuhDkn4Vx8Si6n3QalOevgPTkacLcTruYWbWYO9UDzEXGxb8uDHEuFs7oP3wgk2MsIzqLbslXVRV4m0Xy-eBFcmohhHObFKV9IcEdoBWE2drB2hOHCEz_HLLl10DFCw=w600-h315-p-k" />
                 <h3 class="location-text">Bmekkine, Lebanon</h3></div>
-              <p>Lebanon is a city with some insane amount of history. We visited Byblos, that is one of the oldest cities in the world, as it has been continuously inhabited for the past 7,000 years! The city of Beirut was very unique and the civil war it went through in the 70s-90s has definitely left an impact (you could actually see the bullet holes on the sides of the buildings!). However, these days Beirut has been known as one of the safest cities, so do not hesitate to visit! The architecture and vibe of the city was stunning. 
+              <p>Lebanon is a city with some insane amount of history. We visited Byblos, that is one of the oldest cities 
+              in the world, as it has been continuously inhabited for the past 7,000 years! The city of Beirut was very 
+              unique and the civil war it went through in the 70s-90s has definitely left an impact (you could actually see 
+              the bullet holes on the sides of the buildings!). However, these days Beirut has been known as one of the 
+              safest cities, so do not hesitate to visit! The architecture and vibe of the city was stunning. 
               <div class="img">
                 <img class="square-image" src="https://lh3.googleusercontent.com/m1RyM4iGHCRZjg2cdS93sZxZlXV-LTGjKpSkcUreg3L4rdoF2_RP352VSdm7ZBOxVGwIdKgvYM8kT5Q99ANhKB1dAe6D5yrFtRbY621OuqzmlF2fM8lyu9HyWuhQDlrGDnjSbLRHNMA=w600-h315-p-k" />
                 <h3 class="location-text">Byblos, Lebanon</h3></div>
-              <p>There are so many different districts in the city that are massively different from each other ranging from hipster areas (Mar Michael, Gemmayzeh), to the Armenian district and various Christian and Muslim oriented areas throughout the city. This diversity played out in the night life as well where parties went all night long from underground bomb selter clubs to fancy rooftop night clubs!
+              <p>There are so many different districts in the city that are massively different from each other ranging 
+              from hipster areas (Mar Michael, Gemmayzeh), to the Armenian district and various Christian and Muslim oriented 
+              areas throughout the city. This diversity played out in the nightlife as well where parties went all night long 
+              from underground bomb selter clubs (thank you Wael for showing us a good time!) to fancy rooftop night clubs!
               <div class="img">
                 <img class="square-image" src="https://lh3.googleusercontent.com/sk1wI_9-mIfjFrD2JRnVD2OEHKeWJSwtQ7PpYqJ23xRILOv3uuKyjyjD19X-lcv-jdA7SEOoEEBiJUhLy3AL51c0LyzPWL4CQvjAfShZVHHiyBmkQcpqToH81D5m8Dop0gAla_vvtBk=w600-h315-p-k" />
                 <h3 class="location-text">Beirut, Lebanon</h3></div>
               <div class="img">
                 <img class="square-image" src="https://lh3.googleusercontent.com/FWMALQG4p4_ogvp2iJob8veY4aoxPmGYq8GBfTzQsBRESWmjjVqKOXr65vwF_-ELWwOCziaTCWACeLj3Ai-9cKCWmkv5P_RmI2JXUd9NKt-qfWEL-WAB9nOOpnejRim6YAYZyzJUWlM=w600-h315-p-k" />
                 <h3 class="location-text">Beirut, Lebanon</h3></div>
-              <p>Lebanon in general has a Mediterranean and western vibe to it, and the food was absolutely delicious! Olga could not get enough <a href='https://en.wikipedia.org/wiki/Manakish'>Manouche</a> which can be eaten for breakfast, lunch and dinner! Jack's favorite time of day was breakfast where there was a plethora of chickpeas recipes, meats and yogurt. And of course we could not stay away from the unlimited supply of shawerma (probably one in every corner)! 
+              <p>Lebanon in general has a Mediterranean and western vibe to it, and the food was absolutely delicious! Olga 
+              could not get enough <a href='https://en.wikipedia.org/wiki/Manakish'>Manouche</a> which can be eaten for 
+              breakfast, lunch and dinner! Jack's favorite time of day was breakfast where there was a plethora of chickpeas 
+              recipes, meats and yogurt. And of course we could not stay away from the unlimited supply of shawerma (probably 
+              one in every corner)! 
               <div class="img">
                 <img class="square-image" src="https://lh3.googleusercontent.com/anFPIPHW09VeK2OkUYlBHM20s-LX1ePoo6SEo2xAMRCtOuxTS1Iw2zdzdW655uuW-8LERzX4yB8gZKxwGWGGi6rALku-mFGDd7D32-wfT8ImuR_JhVYp-Gx4EtZ-y3EN2VHz9PUQLG0=w600-h315-p-k" />
                 <h3 class="location-text">Armenian District, Lebanon</h3></div>
