f82cc64191097126f6a798f2d5ac46ddcfb22d54
Olga Kavvada <okavvada@gmail.com>
2018-09-25 15:33:35 +0530

    move files around after processing

--- photos_link_processor.py
+++ photos_link_processor.py
@@ -1,9 +1,11 @@
 import requests
+import os
 
 
 def photo_processor(file_name):
+  processed_filename = file_name + ".processed"
   with open(file_name) as file:
-    with open(file_name + ".processed", 'w') as file_out:
+    with open(processed_filename, 'w') as file_out:
       for line in file:
         if 'app.goo.gl' in line:
           processed_line = processed(line)
@@ -11,6 +13,9 @@ def photo_processor(file_name):
           file_out.write(processed_line + '\n')
         else:
           file_out.write(line)
+  os.rename(file_name, file_name + '.old')
+  os.rename(processed_filename, file_name)
+
 
 def processed(line):
   return requests.get('http://us-central1-taxeedee-212808.cloudfunctions.net/cloud?url=%s' % line.strip()).text
